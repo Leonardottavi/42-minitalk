24c19596ff09343824e81e7570746b1c3931e4b0
Leonardo Ottavi <lottavi@e3r5p7.42roma.it>
2023-05-17 16:37:30 +0200

    lottavi

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:55:52 by lottavi           #+#    #+#             */
-/*   Updated: 2023/05/17 15:51:51 by lottavi          ###   ########.fr       */
+/*   Updated: 2023/05/17 16:36:02 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,43 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	return (0);
+}
+
+static int	ft_calc_num_str(const char *str)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	num = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (i == 0)
+			num = (str[i] - '0');
+		else
+			num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	min;
+
+	i = 0;
+	min = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			min++;
+		i++;
+	}
+	if (min == 1)
+		return (ft_calc_num_str(str + i) * (-1));
+	return (ft_calc_num_str(str + i));
 }
