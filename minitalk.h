7c96a60b76f4e1c24e6a04a144545fdb8ae41839
Leonardo Ottavi <lottavi@e3r5p5.42roma.it>
2023-05-23 10:16:57 +0200

    lottavi

--- minitalk.h
+++ minitalk.h
@@ -5,16 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/17 16:29:12 by lottavi           #+#    #+#             */
-/*   Updated: 2023/05/17 16:29:21 by lottavi          ###   ########.fr       */
+/*   Created: 2023/05/23 10:14:05 by lottavi           #+#    #+#             */
+/*   Updated: 2023/05/23 10:15:30 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../libft/inc/libft.h"
-# include "../libft/inc/ft_printf.h"
 # include <signal.h>
+# include "libft.h"
+
+/*
+** Colors
+*/
+# define ANSI_COLOR_RED		"\x1b[31m"
+# define ANSI_COLOR_BLUE	"\x1b[34m"
+# define ANSI_COLOR_GREEN	"\x1b[32m"
+# define ANSI_COLOR_YELLOW	"\x1b[33m"
+# define ANSI_COLOR_RESET	"\x1b[0m"
 
 #endif
